edebb4f637fd282543da39dc9cacd0cfbd48a9b6
Christian Moustesgård <cph-cm267@cphbusiness.dk>
2021-03-06 15:26:16 +0100

    added third sql script

---
+++ sql_create_user_access.sql
@@ -0,0 +1,15 @@
+drop user if exists davide;
+CREATE user davide WITH PASSWORD 'jw8s0F4';
+
+revoke all privileges on public.bird from davide;
+revoke all privileges on public.caretake_pet_ref from davide;
+revoke all privileges on public.caretaker from davide;
+revoke all privileges on public.cat from davide;
+revoke all privileges on public.city from davide;
+revoke all privileges on public.dog from davide;
+revoke all privileges on public.fish from davide;
+revoke all privileges on public.horse from davide;
+revoke all privileges on public.pet from davide;
+revoke all privileges on public.reptile from davide;
+revoke all privileges on public.turtle from davide;
+revoke all privileges on public.vet from davide;
